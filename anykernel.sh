e289a5946b492e799c30a2bba51c1dac4e2ab071
Forenche <prahul2003@gmail.com>
2020-08-21 20:16:13 +0530

    anykernel.sh: update for rmx1851

--- anykernel.sh
+++ anykernel.sh
@@ -4,33 +4,34 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=IllusionX kernel
+kernel.string=Stormbreaker-r1 kernel for Realme 3 Pro
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=RMX1851
 device.name2=rmx1851
+device.name3=Realme 3 Pro
+device.name4=realme 3 pro
 supported.versions=10
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=none;
+ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
 ## AnyKernel install
-# dump_boot;
-split_boot;
+dump_boot;
 
-if [ -f $split_img/ramdisk.cpio ]; then
-  unpack_ramdisk;
-  repack_ramdisk;
-fi;
+# begin ramdisk changes
+# end ramdisk changes
 
-flash_boot;
-flash_dtbo;
+write_boot;
+## end install
+
